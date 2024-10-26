b6a6c0cd40e349e78d391d9c60d75ccd652342e6
User <alimuradof93@gmail.com>
2024-10-26 14:42:55 +0400

    homework

---
+++ Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte b=5;
+        short a=4;
+        int c=8;
+        long d=7;
+        float f=8.57f;
+    System.out.println(a);
+    System.out.printf("%d",b);
+    System.out.print("  ");
+    System.out.printf("%.2f",f);
+
+    }
+    }
